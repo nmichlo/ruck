228a09a37c04ee300883ec64e5b73fd5ad770269
Nathan Michlo <NathanJMichlo@gmail.com>
2021-09-25 12:00:02 +0200

    additional python version support

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setuptools.setup(
     author_email="NathanJMichlo@gmail.com",
 
     version="0.0.1.dev1",
-    python_requires=">=3.8",
+    python_requires=">=3.6",
     packages=setuptools.find_packages(),
 
     install_requires=install_requires,
@@ -62,7 +62,10 @@ setuptools.setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Intended Audience :: Science/Research",
     ],
 )
